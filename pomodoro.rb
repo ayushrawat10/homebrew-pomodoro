dd86871c6ef34e9e7832ec9a4fb13c4f7cab36c0
Ayush Rawat <ayush.rawat3@gmail.com>
2023-10-07 19:28:58 -0700

    Changed the formula

--- pomodoro.rb
+++ pomodoro.rb
@@ -1,8 +1,8 @@
 class Pomodoro < Formula
   desc "A CLI pomodoro timer"
   homepage "https://github.com/ayushrawat10/pomodoro-timer-cli.git"
-  url :"https://github.com/ayushrawat10/pomodoro-timer-cli.git/archive/refs/tags/v1.0.tar.gz"
-  sha256 ""
+  url :"https://github.com/ayushrawat10/pomodoro-timer-cli.git/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "b1d6435b50567c7ca31e608b5b5ab22974110edfc0b4825a727eb1d39f1f6935"
 
   def install
     system "make"
